52e785c33a7e9ec139ad628245429ac0f6d3ff0a
root <root@LAPTOP-ERU2II9C.>
2025-05-07 16:26:28 +0700

    catatan pertemuan 5

---
+++ pert4/python/percobaan.py
@@ -0,0 +1,10 @@
+def tambah(nilai1, nilai2):
+    print(nilai1+nilai2)
+def kurang(nilai1, nilai2):
+    print(nilai1-nilai2)
+
+tambah(10,20)
+tambah(1,2)
+tambah(100,40)
+tambah(10,20)
+kurang(10,2)
